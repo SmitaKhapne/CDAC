b30198ca512ea1441f5bcf227e6f3054beb8993b
SmitaKhapne <smitakhapne0@gmail.com>
2024-08-24 12:04:07 +0530

    Delete Assignment/Assignment1/Program directory

--- Assignment/Assignment1/Program/AreaOfRectangle.java
+++
@@ -1,9 +0,0 @@
-class AreaOfRectangle
-{
-   public static void main(String[] args)
-   {  
-      int length = 10;
-      int width = 5;
-      System.out.println("Area Of Square : " + (length * width));
-   }
-}
--- Assignment/Assignment1/Program/AreaOfSquare.java
+++
@@ -1,8 +0,0 @@
-class AreaOfSquare
-{
-   public static void main(String[] args)
-   {  
-      int side = 10;
-      System.out.println("Area Of Square : " + (side*side));
-   }
-}

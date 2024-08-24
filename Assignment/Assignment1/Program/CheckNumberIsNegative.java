9fd34c07e3ab34999b53cb181e29cde81a0fd29b
SmitaKhapne <smitakhapne0@gmail.com>
2024-08-24 12:08:53 +0530

    uploaded program assignment1

---
+++ Assignment/Assignment1/Program/CheckNumberIsNegative.java
@@ -0,0 +1,15 @@
+class CheckNumberIsNegative
+{ 
+   public static void main(String[] args)
+   {  
+      int num = -8;
+      if(num < 0)
+      {
+         System.out.println("Number is NEGATIVE");
+      }
+       else
+       {
+          System.out.println("Invalid");
+       }
+     }
+}
